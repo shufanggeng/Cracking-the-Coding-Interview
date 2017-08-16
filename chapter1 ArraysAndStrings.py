9b70402f697fe471783d227984aa4b2e0a95f10a
shufanggeng <shufanggeng@gmail.com>
2017-08-16 10:25:29 -0400

    Update chapter1 ArraysAndStrings.py

--- chapter1 ArraysAndStrings.py
+++ chapter1 ArraysAndStrings.py
@@ -1,7 +1,9 @@
 # -*- coding: utf-8 -*-
 
-""" 1.1 Implement an algorithm to determine if a string has all unique 
-    characters. What if you cannot use additional data structure """
+""" 
+1.1 Implement an algorithm to determine if a string has all unique 
+    characters. What if you cannot use additional data structure 
+"""
     
 def uniqueString(str):
     if not str:
@@ -10,8 +12,10 @@ def uniqueString(str):
     string """
     return len(set(str)) == len(str)
 
-""" 1.3 Given two strings,write a method to decide if one is a permutation 
-    of the other """
+""" 
+1.3 Given two strings,write a method to decide if one is a permutation 
+    of the other 
+"""
     
 def permutation(str1, str2):
     if not str1 or not str2:
@@ -27,10 +31,12 @@ def permutation1(str1, str2):
     """count characters of two strings"""
     return collections.Counter(str1) == collections.Counter(str2)
 
-""" 1.4 Write a method to replace all spaces in a string with '%20'. 
+""" 
+1.4 Write a method to replace all spaces in a string with '%20'. 
     You may assume that the string has sufficient space at the end of the 
     string to hold the additional characters, and that you are given the 
-    "true" length of the string."""
+    "true" length of the string.
+"""
 
 def replaceSpaces(str):
     s = list(str)
@@ -62,10 +68,12 @@ def replaceSpaces1(str):
             index -= 1
     return l
 
-""" 1.5 Implement a method to perform basic string compression using the 
+""" 
+1.5 Implement a method to perform basic string compression using the 
     counts of repeated characters. For example, the string 'aabcccccaaa'
     would become a2b1c5a3. If the "compressed" string would not become smaller 
-    than the original string, your method should return the original string"""
+    than the original string, your method should return the original string
+"""
     
 def compress(inputStr):
     if not inputStr:
@@ -96,9 +104,11 @@ def compress(inputStr):
     else:
         return outputStr
     
-""" 1.6 Given an image represented by an NxN matrix, where each pixel in the 
+""" 
+1.6 Given an image represented by an NxN matrix, where each pixel in the 
     image is 4 bytes, write a method to rotate the image by 90 degrees. 
-    Can you do this in place? """
+    Can you do this in place? 
+"""
     
 def rotateImage(matrix):
     n = len(matrix)
@@ -109,19 +119,23 @@ def rotateImage(matrix):
             matrix[n-j-1][i],matrix[n-i-1][n-j-1], matrix[j][n-i-1], matrix[i][j]
      
        
-""" Clock-wise or counter-clock-wise
+""" 
+Clock-wise or counter-clock-wise
 Clock-wise, two steps: 1.transpose; 2. Reverse
 Zip(*original[::-1])
 
 Counter-clock-wise: 1.reverse (flip the matrix upside down); 2. Transpose
 Reversed = original[::-1]
-Zipped = zip(*reversed) """
+Zipped = zip(*reversed) 
+"""
 
 def rotateImage1(matrix):
     return matrix[:] = zip(*matrix[::-1])
       
-""" 1.7 Write an algorithm such that if an element in an MxN matrix is 0, 
-    its entire row and column are set to 0 """
+""" 
+1.7 Write an algorithm such that if an element in an MxN matrix is 0, 
+    its entire row and column are set to 0 
+"""
 
 def zeroMatrix(matrix):
      m = len(matrix)
@@ -145,10 +159,12 @@ def zeroMatrix(matrix):
              if matrix[i][j] == 'a':
                  matrix[i][j] = 0
         
-""" 1.8 Assume you have a method isSubstring which checks if one word is 
+""" 
+1.8 Assume you have a method isSubstring which checks if one word is 
     a substring of another. Given two strings, s1 and s2, write code to check 
     If s2 is a rotation of s1 using only one call to isSubstring 
-    (e.g., "waterbottLe" is a rotation of "erbot- tLewat"). """
+    (e.g., "waterbottLe" is a rotation of "erbot- tLewat"). 
+"""
     
 def isRotation(s1, s2):
     if len(s1) != len(s2):
